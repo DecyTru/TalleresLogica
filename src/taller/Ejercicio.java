8c270ded343fc83e9fa90c514c4f9c841ba48735
Manuc6 <decyclox282@gmail.com>
2023-08-15 17:01:14 -0500

    ci
    ci

--- src/taller/Ejercicio.java
+++ src/taller/Ejercicio.java
@@ -28,7 +28,7 @@ public class Ejercicio {
         boolean d= 25>18;
         System.out.println("Resultado " + d);
 
-        
+        /*Ejercicio 5*/
          double e= 3+(2.5*4)/3;
          
          double f= (4*5.2)-3.8;
@@ -36,6 +36,23 @@ public class Ejercicio {
          System.out.println("Resultado operacion 1: " +e);
          System.out.println("Resultado operacion 2: " +f);
          System.out.println("Si los valores no son iguales da verdadero de lo contrario da falso:  " +r);
+         
+         /*Ejercicio 6*/
+        double g= (8.5/3.2)+(6.5-(2.3*5)/6);
+        System.out.println("el primero" + g);
+        double h= 15.8;
+         System.out.println("el primero" + h);
+        boolean i= g >= h;
+        System.out.println("Resultado operación 1: " + i);
+        double j= 21.4;
+        System.out.println("Resultado " + j);
+        double k= (81/4)+2; 
+        System.out.println("Segundo "+k);
+        boolean l= j<k;
+        boolean m= i && l;
+        
+        System.out.println("Resultado operación 2: " + l);
+        System.out.println("Resultado operación 1: " + m);
 
     }
     
